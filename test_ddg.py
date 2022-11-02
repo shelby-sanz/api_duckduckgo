9366d8a3c54ccbedfadb4d34f4148a228e46c3c4
shelby <shelbyy.herrera@gmail.com>
2022-11-02 15:09:05 -0400

    cleaned up code and added comments

--- test_ddg.py
+++ test_ddg.py
@@ -2,11 +2,7 @@ import requests
 import pytest
 
 
-# def get_answers(search_words):
-#     url = ''
-#
-
-
+# make api call to duckduckgo
 def make_call():
     # make api request and read as json (dict)
     search_string = 'presidents of the united states'
@@ -22,6 +18,7 @@ def make_call():
     return texts
 
 
+# list of president last names
 president_last_names = ['washington', 'adams', 'jefferson', 'madison', 'monroe', 'adams', 'jackson', 'buren',
                         'harrison', 'tyler', 'polk', 'taylor', 'fillmore', 'pierce', 'buchanan', 'lincoln',
                         'johnson', 'grant', 'hayes', 'garfield', 'arthur', 'cleveland', 'harrison', 'cleveland',
@@ -30,6 +27,7 @@ president_last_names = ['washington', 'adams', 'jefferson', 'madison', 'monroe',
                         'clinton', 'bush', 'obama', 'trump', 'biden']
 
 
+# pytest to assert if each president last name is found in duckduckgo response
 @pytest.mark.parametrize("item", president_last_names)
 def test_president_names(item):
     texts = make_call()
